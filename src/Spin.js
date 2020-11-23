7d2495af8eec35e8b6a3c83bbce0d962d48e40fb
Mito6211 <willmoore674@gmail.com>
2020-11-22 22:15:52 -0500

    Added some basic animations

--- src/Spin.js
+++ src/Spin.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-
+import "animate.css";
 import styles from "./Spin.module.css"
 
 const Spin = ({ data: { totalPercentage, tiers } }) => {
 
-    const [rarity, setRarity] = useState({tier: "RARITY"})
+    const [rarity, setRarity] = useState({ tier: "RARITY" })
     const [item, setItem] = useState("ITEM")
 
 
@@ -16,27 +16,29 @@ const Spin = ({ data: { totalPercentage, tiers } }) => {
             }
         })
 
-        const randomTier = allTiers[Math.floor(Math.random()*allTiers.length)];
+        const randomTier = allTiers[Math.floor(Math.random() * allTiers.length)];
 
         setRarity(randomTier);
         console.log(randomTier);
         // there will be an animation here
         // then it wait for a few seconds
 
-        setItem(randomTier.items[Math.floor(Math.random()*randomTier.items.length)])
+        setItem(randomTier.items[Math.floor(Math.random() * randomTier.items.length)])
 
     }
 
     return (
         <>
             <div className={styles.spinMainContext}>
-                <div className={styles.dataBox}><span>{rarity.tier}</span></div>
-                <div className={styles.dataBox}><span>{item}</span></div>
+                <div className={styles.dataBox}><span className="animate__animated animate__zoomIn">{rarity.tier}</span></div>
+                <div className={styles.dataBox}><span className="animate__animated animate__zoomIn">{item}</span></div>
             </div>
             <div className={styles.otherData}>
-                <button className={styles.spinBtn} onClick={pickItem} disabled={totalPercentage !== 100}>SPIN!</button>
+                <button className={styles.spinBtn} onClick={pickItem} disabled={totalPercentage !== 100}>
+                    <span className="animate__animated animate__zoomIn">SPIN!</span>
+                </button>
                 <div className={styles.outcome} style={{ color: totalPercentage === 100 ? "green" : "red" }}>
-                    The outcome is at {totalPercentage}%!
+                    <span className="animate__animated animate__zoomIn">The outcome is at {totalPercentage}%!</span>
                 </div>
             </div>
         </>
